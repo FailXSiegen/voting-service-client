d8b4fc0f09f05dd6b4c5833fa220e392eb75164e
moritzpreuss <m.preuss@psv-neo.de>
2020-08-08 10:51:49 +0200

    add styles to navigation

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -21,3 +21,25 @@ export default {
   }
 }
 </script>
+
+<style scoped>
+  .list-group-item {
+    border-color: transparent;
+    border-width: 3px;
+  }
+  .list-group-item.list-group-item-dark.router-link-exact-active,
+  .list-group-item.list-group-item-dark:active,
+  .list-group-item.list-group-item-dark:focus {
+    background-color: white;
+    border-color: var(--primary);
+    border-width: 3px;
+  }
+  .list-group-item.list-group-item-dark:hover {
+    border-color: transparent;
+    background-color: var(--primary);
+    color: white;
+  }
+  .list-group-item+.list-group-item {
+    border-top-width: initial;
+  }
+</style>
