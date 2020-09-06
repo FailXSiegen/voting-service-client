bb72ba0e3756d903bf1d36b8cebe06ccf2e9e75c
habundloot <mo@habundloot.de>
2020-09-06 19:39:15 +0200

    add log pout add/logout-button

--- src/views/admin/Profile.vue
+++ src/views/admin/Profile.vue
@@ -1,10 +1,10 @@
 <template>
   <div class="profile-container container-fluid">
     <div class="row">
-      <div class="col-12 col-md-3 bg-dark text-white py-3 order-2 order-lg-1">
+      <div class="col-12 col-md-3 bg-dark text-white py-3 order-2 order-md-1">
         <app-navigation />
       </div>
-      <div class="col-12 col-md-5 py-3 order-2 order-lg-1">
+      <div class="col-12 col-md-5 py-3 order-2 order-md-1">
         <h1>{{ headline }}</h1>
         <p>{{ username }}</p>
         <app-form />
