0a15e5f0c081db1cd96c3f6ecd07793eba5210dd
FailXSiegen <info@failx.de>
2020-09-22 20:56:06 +0200

    Feature/polls with no results (#39)
    * add querie poll type and polls with no results
    
    * add POLLS_WITH_NO_RESULTS to organizer poll view
    
    Co-authored-by: Felix Herrmann <felix.herrmann@riconet.de>

--- src/components/events/event/polls/PollsListing.vue
+++ src/components/events/event/polls/PollsListing.vue
@@ -1,8 +1,8 @@
 <template>
- <div class="polls-listing-container">
+ <div class="polls-listing-container" v-if="pollsWithNoResults">
    <h2>{{ localize('view.polls.headlines.listingTitle') }}</h2>
    <ul class="created-polls list-group">
-     <li v-for="(poll, index) in polls" :key="index" class="list-group-item">
+     <li v-for="(poll, index) in pollsWithNoResults" :key="index" class="list-group-item">
        <h5 class="mb-1">{{ poll.title }}</h5>
        <button class="btn btn-primary my-2 mr-2" @click="startPoll">
          <i class="bi-play bi--2xl align-middle"></i>
@@ -22,34 +22,10 @@
 import { localize } from '@/helper/localization-helper'
 
 export default {
-  data () {
-    return {
-      polls: {
-        poll1: {
-          id: 1,
-          title: 'Title Poll 1',
-          pollAnswer: 'yesNoAbstain',
-          custom: {
-            list: '',
-            allowAbstain: false,
-            minVotes: 1,
-            maxVotes: 1
-          },
-          pollType: 'public'
-        },
-        poll2: {
-          id: 2,
-          title: 'Another Title Poll 2',
-          pollAnswer: 'yesNo',
-          custom: {
-            list: '',
-            allowAbstain: false,
-            minVotes: 1,
-            maxVotes: 1
-          },
-          pollType: 'public'
-        }
-      }
+
+  props: {
+    pollsWithNoResults: {
+      type: Array
     }
   },
   methods: {
--- src/views/admin/event/Polls.vue
+++ src/views/admin/event/Polls.vue
@@ -7,7 +7,7 @@
       <div class="col-12 col-md-9 py-3 order-1 order-lg-2">
         <h1>{{ headline }}</h1>
         <hr>
-        <app-polls :eventRecord="eventRecord" />
+        <app-polls :pollsWithNoResults="pollsWithNoResults" :eventRecord="eventRecord" />
       </div>
     </div>
   </div>
@@ -19,7 +19,7 @@ import AppNavigation from '@/components/events/event/Navigation'
 import AppPolls from '@/components/events/event/Polls'
 import { localize } from '@/helper/localization-helper'
 import { fetchEventBySlug } from '@/api/event'
-import { EVENT_USERS_BY_EVENT } from '@/graphql/queries'
+import { EVENT_USERS_BY_EVENT, POLLS_NO_RESULTS } from '@/graphql/queries'
 import { NEW_EVENT_USER_SUBSCRIPTION } from '@/graphql/subscriptions'
 
 export default {
@@ -48,6 +48,14 @@ export default {
         }
       }
     },
+    pollsWithNoResults: {
+      query: POLLS_NO_RESULTS,
+      variables () {
+        return {
+          eventId: this.eventRecord.id
+        }
+      }
+    },
     $subscribe: {
       newEventUser: {
         query: NEW_EVENT_USER_SUBSCRIPTION,
@@ -64,7 +72,8 @@ export default {
     return {
       headline: 'Umfragen',
       eventRecord: {},
-      eventUsers: []
+      eventUsers: [],
+      pollsWithNoResults: []
     }
   },
   methods: {
