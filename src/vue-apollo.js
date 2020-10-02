287c684838380e00a90cfc1855a88a2fb7468962
Wolf Utz <wpu@hotmail.de>
2020-10-02 21:43:13 +0200

    Fix websocket auth logic

--- src/vue-apollo.js
+++ src/vue-apollo.js
@@ -1,89 +1,151 @@
+import { handleGraphQlErrors } from './error/handler/apollo-error-handler'
+import { onError } from 'apollo-link-error'
+import { logErrorMessages } from '@vue/apollo-util'
+import { restartWebsockets } from 'vue-cli-plugin-apollo/graphql-client'
 import Vue from 'vue'
 import VueApollo from 'vue-apollo'
-import { createApolloClient, restartWebsockets } from 'vue-cli-plugin-apollo/graphql-client'
+import { ApolloClient } from 'apollo-client'
+import { HttpLink } from 'apollo-link-http'
+import { InMemoryCache } from 'apollo-cache-inmemory'
+import { split, ApolloLink } from 'apollo-link'
+import { WebSocketLink } from 'apollo-link-ws'
+import { getMainDefinition } from 'apollo-utilities'
 import { TokenRefreshLink } from 'apollo-link-token-refresh'
-import { ApolloLink } from 'apollo-link'
-import { getCurrentUnixTimeStamp } from '@/lib/time-stamp'
 import { jwtDecode } from '@/lib/jwt-util'
 import router from '@/router'
 import { refreshLogin } from '@/graphql/auth'
-import { apolloProvider } from '@/main'
+import { getCurrentUnixTimeStamp } from '@/lib/time-stamp'
 
-// Install the vue plugin
-Vue.use(VueApollo)
-
-// Name of the localStorage item
 export const AUTH_TOKEN = 'apollo-token'
 
-// Http endpoint (use httpsEndpoint in production)
-const httpEndpoint = process.env.VUE_APP_GRAPHQL_HTTP
+const uriHttp = process.env.VUE_APP_GRAPHQL_ENDPOINT
+const uriWs = process.env.VUE_APP_WS_ENDPOINT
 
-const link = ApolloLink.from([
-  new TokenRefreshLink({
-    isTokenValidOrUndefined: () => {
-      const token = localStorage.getItem(AUTH_TOKEN)
-      if (token === null) {
-        router.push('/').catch(() => {})
-      }
-      const decodedToken = jwtDecode(token)
-      return decodedToken.payload.exp > getCurrentUnixTimeStamp()
-    },
-    fetchAccessToken: () => {
-      return refreshLogin()
-    },
-    handleFetch: async accessToken => {
-      console.info('silent refresh!')
-      localStorage.setItem(AUTH_TOKEN, accessToken)
-      await apolloProvider.defaultClient.cache.reset()
-    },
-    handleResponse: (operation, accessTokenField) => async response => {
-      return {
-        access_token: response.token
-      }
-    },
-    handleError: err => {
-      console.error(err)
+const headers = { authorization: getAuth() }
+
+const httpLink = new HttpLink({ uri: uriHttp, headers })
+const wsLink = new WebSocketLink({
+  uri: uriWs,
+  options: {
+    reconnect: true,
+    lazy: true,
+    connectionParams () {
+      return { headers }
     }
-  })
-])
+  }
+})
 
-// Config
-const defaultOptions = {
-  httpEndpoint,
-  wsEndpoint: process.env.VUE_APP_WS_ENDPOINT,
-  tokenName: AUTH_TOKEN,
-  persisting: false,
-  websocketsOnly: false,
-  ssr: false,
-  link
+const refreshTokenLink = new TokenRefreshLink({
+  isTokenValidOrUndefined: () => {
+    const token = localStorage.getItem(AUTH_TOKEN)
+    if (token === null) {
+      router.push('/').catch(() => {})
+    }
+    const decodedToken = jwtDecode(token)
+    return decodedToken.payload.exp > getCurrentUnixTimeStamp()
+  },
+  fetchAccessToken: () => {
+    return refreshLogin()
+  },
+  handleFetch: async accessToken => {
+    localStorage.setItem(AUTH_TOKEN, accessToken)
+    await apolloProvider.defaultClient.cache.reset()
+  },
+  handleResponse: (operation, accessTokenField) => async response => {
+    return {
+      access_token: response.token
+    }
+  },
+  handleError: err => {
+    console.error(err)
+  }
+})
+
+const errorLink = onError(error => {
+  logErrorMessages(error)
+  handleGraphQlErrors(error)
+})
+
+wsLink.subscriptionClient.on('connecting', () => {
+  if (process.env.VUE_APP_VERBOSE === '1') {
+    console.info('connecting to websocket')
+  }
+})
+
+wsLink.subscriptionClient.on('connected', () => {
+  if (process.env.VUE_APP_VERBOSE === '1') {
+    console.info('connected to websocket')
+  }
+})
+
+wsLink.subscriptionClient.on('reconnecting', () => {
+  if (process.env.VUE_APP_VERBOSE === '1') {
+    console.info('reconnecting to websocket')
+  }
+})
+
+wsLink.subscriptionClient.on('reconnected', () => {
+  if (process.env.VUE_APP_VERBOSE === '1') {
+    console.info('reconnected to websocket')
+  }
+})
+
+wsLink.subscriptionClient.on('disconnected', () => {
+  if (process.env.VUE_APP_VERBOSE === '1') {
+    console.info('disconnected to websocket')
+  }
+})
+
+wsLink.subscriptionClient.maxConnectTimeGenerator.duration = () => wsLink.subscriptionClient.maxConnectTimeGenerator.max
+
+// combine http and ws link
+const splitLink = split(({ query }) => {
+  const { kind, operation } = getMainDefinition(query)
+  return kind === 'OperationDefinition' && operation === 'subscription'
+}, wsLink, httpLink)
+
+export const defaultClient = new ApolloClient({
+  link: ApolloLink.from([
+    refreshTokenLink, errorLink, splitLink
+  ]),
+  cache: new InMemoryCache(),
+  connectToDevTools: true
+})
+
+const apolloProvider = new VueApollo({
+  defaultClient,
+  defaultOptions: {
+    $loadingKey: 'loading'
+  }
+})
+
+Vue.use(VueApollo)
+
+function getAuth () {
+  if (typeof localStorage !== 'undefined') {
+    const token = localStorage.getItem(AUTH_TOKEN)
+    return `Bearer ${token}`
+  }
+  return ''
 }
 
-export function createProvider (options = {}) {
-  const { apolloClient, wsClient } = createApolloClient({
-    ...defaultOptions,
-    ...options
-  })
-  apolloClient.wsClient = wsClient
-
-  return new VueApollo({
-    defaultClient: apolloClient,
-    defaultOptions: {
-      $query: {
-        // fetchPolicy: 'cache-and-network',
-      }
-    },
-    errorHandler (error) {
-      // eslint-disable-next-line no-console
-      console.log('%cError', 'background: red; color: white; padding: 2px 4px; border-radius: 3px; font-weight: bold;', error.message)
-    }
-  })
+function recoverWsClient (apolloClient) {
+  headers.authorization = getAuth()
+  if (apolloClient.wsClient) {
+    restartWebsockets(apolloClient.wsClient)
+    return
+  }
+  apolloClient.wsClient = wsLink.subscriptionClient
+  restartWebsockets(apolloClient.wsClient)
 }
+
+export default apolloProvider
 
 export async function onLogin (apolloClient, token) {
   if (typeof localStorage !== 'undefined' && token) {
     localStorage.setItem(AUTH_TOKEN, token)
   }
-  if (apolloClient.wsClient) restartWebsockets(apolloClient.wsClient)
+  recoverWsClient(apolloClient)
   try {
     await apolloClient.resetStore()
   } catch (e) {
@@ -111,7 +173,7 @@ export async function onLogout (apolloClient) {
   if (typeof localStorage !== 'undefined') {
     localStorage.removeItem(AUTH_TOKEN)
   }
-  if (apolloClient.wsClient) restartWebsockets(apolloClient.wsClient)
+  recoverWsClient(apolloClient)
   try {
     await apolloClient.resetStore()
   } catch (e) {
