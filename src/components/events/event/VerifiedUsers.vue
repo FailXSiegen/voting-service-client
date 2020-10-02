73b836ca40b82c49f44d2dd3d91042bb1aa1ef9e
habundloot <mo@habundloot.de>
2020-10-02 23:48:35 +0200

    add pulsing user Information on click to code

--- src/components/events/event/VerifiedUsers.vue
+++ src/components/events/event/VerifiedUsers.vue
@@ -6,29 +6,32 @@
         <div class="list-group-content">
           <div class="list-group-info">
             <div class="user-information">
-              <div class="mb-1">
+              <h5 class="mb-1">
+                {{ user.publicName }}
                 <span class="badge badge-success badge-pill status-indicator" v-if="user.online">online</span>
                 <span class="badge badge-danger badge-pill status-indicator" v-else>offline</span>
-                <h5>{{ user.publicName }}</h5>
-                <p class="text-success small" v-if="user.allowToVote">
-                  {{ localize('view.event.user.member') }}</p>
+                <span class="text-success small" v-if="user.allowToVote">
+                  {{ localize('view.event.user.member') }}</span>
                 <span class="text-info small" v-else>{{ localize('view.event.user.visitor') }}</span>
-              </div>
+              </h5>
+              <p class="mb-0">{{ user.username }}
+                <span v-if="user.voteAmount">
+                    - Stimmen: {{ user.voteAmount }}
+                  </span>
+              </p>
             </div>
-            <p class="mb-0">{{ user.username }} <span
-              v-if="user.voteAmount">- Stimmen: {{ user.voteAmount }}</span>  </p>
           </div>
           <div class="d-flex justify-content-center align-items-center btn-group">
-            <button v-if="!user.allowToVote" v-on:click="updateToParticipant(user)" class="h-100 btn btn-success m-2">
+            <button v-if="!user.allowToVote" v-on:click="updateToParticipant(user)" class="h-100 btn btn-success">
               {{ localize('view.event.user.setTo') }}
               {{ localize('view.event.user.member') }}
             </button>
-            <button v-else-if="user.allowToVote" v-on:click="updateUserToGuest(user)" class="h-100 btn btn-info m-2">
+            <button v-else-if="user.allowToVote" v-on:click="updateUserToGuest(user)" class="h-100 btn btn-info">
               {{ localize('view.event.user.setTo') }}
               {{ localize('view.event.user.visitor') }}
             </button>
             <router-link :to="{ name: 'UpdateEventUser', params: { eventUserRecord: user } }"
-                         class="h-100 btn float-xl-right btn-warning d-flex justify-content-center align-items-center">
+                         class="btn h-100 btn-warning d-flex justify-content-center align-items-center">
               <i class="bi-pencil align-middle"></i>
             </router-link>
           </div>
@@ -94,32 +97,40 @@ export default {
   margin-top: -2px;
 }
 
-h5 {
-}
-
 .list-group-content {
   display: flex;
   width: 100%;
-  justify-content: space-between;
-  align-content: center;
   margin-bottom: 1rem;
   flex-direction: column;
 }
 
 .list-group-info {
+  width: 100%;
   display: flex;
   flex-direction: column;
   padding-right: 1.5rem;
+}
+
+.user-information {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
 }
 
 @media all and (min-width: 567px) {
   .list-group-content {
     justify-content: space-between;
     flex-direction: row;
+    margin-bottom: 0;
+  }
+
+  .btn-group .btn {
+    font-size: .8rem;
   }
 
   .list-group-info {
     flex-direction: column;
+    justify-content: center;
   }
 }
 </style>
