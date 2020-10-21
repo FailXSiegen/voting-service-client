569c397584964f406942f9910ca9bf149982f55c
0m3gaC0d3 <41939129+0m3gaC0d3@users.noreply.github.com>
2020-10-21 22:42:40 +0200

    Add scss loader (#64)

--- src/main.js
+++ src/main.js
@@ -2,11 +2,9 @@ import Vue from 'vue'
 import App from '@/App.vue'
 import router from '@/router'
 import store from '@/store'
-import 'bootstrap'
-import 'bootstrap/dist/css/bootstrap.min.css'
-import 'bootstrap-icons-font/dist/bootstrap-icons-font.min.css'
 import apolloProvider from '@/vue-apollo.js'
-
+import '@/assets/scss/main.scss'
+import 'bootstrap-icons-font/dist/bootstrap-icons-font.css'
 Vue.config.productionTip = false
 
 // Fetch user data, if present.
