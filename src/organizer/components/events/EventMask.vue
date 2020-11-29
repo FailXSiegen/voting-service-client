dc357d8a0232f7fa32593df65e8b0bedffad161d
Felix Herrmann <felix.herrmann@riconet.de>
2020-11-29 19:12:12 +0100

    add datetime-picker and use in create and update event view

--- src/organizer/components/events/EventMask.vue
+++ src/organizer/components/events/EventMask.vue
@@ -17,8 +17,8 @@
       </small>
     </div>
     <div class="form-group">
-      <label for="scheduledDatetime">{{ localize('view.event.create.labels.scheduledDatetime') }}</label>
-      <input v-model="eventRecord.scheduledDatetime" class="form-control" type="datetime-local" id="scheduledDatetime" required="required">
+      <label>{{ localize('view.event.create.labels.scheduledDatetime') }}</label>
+      <date-picker v-model="eventRecord.scheduledDatetime" :config="options"></date-picker>
     </div>
     <div class="form-check">
       <input v-model="eventRecord.lobbyOpen" class="form-check-input" type="checkbox" id="lobbyOpen">
@@ -43,12 +43,36 @@
 
 import { localize } from '@/frame/lib/localization-helper'
 import { convertUnixTimeStampForDatetimeLocaleInput } from '@/frame/lib/time-stamp'
+import datePicker from 'vue-bootstrap-datetimepicker'
+import moment from 'moment'
 
 export default {
+  components: {
+    datePicker
+  },
   props: {
     eventRecord: {
       type: Object,
       required: true
+    }
+  },
+  data () {
+    return {
+      options: {
+        format: 'DD.MM.YYYY, HH:mm',
+        locale: 'de',
+        icons: {
+          time: 'bi-clock',
+          date: 'bi-calendar',
+          up: 'bi-arrow-up',
+          down: 'bi-arrow-down',
+          previous: 'bi-arrow-left',
+          next: 'bi-arrow-right',
+          today: 'bi-calendar-date',
+          clear: 'bi-trash',
+          close: 'bi-x-circle'
+        }
+      }
     }
   },
   async created () {
@@ -57,7 +81,7 @@ export default {
   methods: {
     convertScheduledDatetime () {
       if (this.eventRecord.scheduledDatetime) {
-        return Math.round(new Date(this.eventRecord.scheduledDatetime).getTime() / 1000)
+        return moment(this.eventRecord.scheduledDatetime, 'DD.MM.YYYY, HH:mm').unix()
       }
       return 0
     },
