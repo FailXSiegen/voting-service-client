594c5317ccbf78442f947ca5f4542f209f6a9b56
FailXSiegen <info@failx.de>
2021-08-10 14:40:37 +0200

    minor html optimization

--- src/organizer/components/events/detail/video-conference-config/ZoomConfig.vue
+++ src/organizer/components/events/detail/video-conference-config/ZoomConfig.vue
@@ -1,9 +1,23 @@
 <template>
   <div v-if="resolvedConfig">
-    <label for="meetingId">Meeting id*</label>
-    <input id="meetingId" v-model="resolvedConfig.credentials.id" @keyup="onChange" class="form-control"/>
-    <label for="meetingPassword">Meeting password*</label>
-    <input id="meetingPassword" v-model="resolvedConfig.credentials.password" @keyup="onChange" class="form-control"/>
+    <div class="form-group">
+      <label for="meetingId">Meeting id*</label>
+      <input
+        id="meetingId"
+        v-model="resolvedConfig.credentials.id"
+        @keyup="onChange"
+        class="form-control"
+      />
+    </div>
+    <div class="form-group">
+      <label for="meetingPassword">Meeting password*</label>
+      <input
+        id="meetingPassword"
+        v-model="resolvedConfig.credentials.password"
+        @keyup="onChange"
+        class="form-control"
+      />
+    </div>
   </div>
 </template>
 
