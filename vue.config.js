1400d4a467897db2ad6098af9f2af8225c01529d
FailXSiegen <info@failx.de>
2021-08-16 21:52:51 +0200

    add copy in webpack and remove script in package

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,4 @@
+const path = require('path')
 module.exports = {
   pluginOptions: {
     'style-resources-loader': {
@@ -19,6 +20,16 @@ module.exports = {
       .tap(options => {
         options.fallback.options.name = '../../fonts/[name].[ext]'
         return options
+      })
+    config.plugin('copy')
+      .tap(args => {
+        args[0].push({
+          from: path.resolve(__dirname, 'node_modules/@zoomus/websdk/dist/lib'),
+          to: path.resolve(__dirname, 'dist/lib/zoom/lib'),
+          toType: 'dir',
+          ignore: ['.DS_Store']
+        })
+        return args
       })
   }
 }
