ef2614568a9aedf7a62686317c2552f84748f091
FailXSiegen <info@failx.de>
2022-02-05 18:01:25 +0100

    update webpack config

--- vue.config.js
+++ vue.config.js
@@ -26,7 +26,7 @@ module.exports = {
             {
               loader: 'url-loader',
               options: {
-                limit: 8192
+                encoding: false
               }
             }
           ]
